9968fc75905e86c35920023e728a693608dd075e
Tairah <Tairah.akintoye@ocsbstudent.ca>
2021-04-01 02:31:07 +0000

    fixed errors.

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 let diameter = 0
-let area = 0
-let circumference = 0
-let Pi = 3.14159265
+const area = 0
+const circumference = 0
+const Pi = 3.14159265
 
-document.getElementById('output').addEventListener('click',click)
+document.getElementById('output').addEventListener('click', click)
 
-function click() {
-  input = document.getElementById('output').value
+function click () {
+  diameter = document.getElementById('output').value
   diameter = parseInt(diameter)
-  if (isNaN(output))
-  output.textContent="You did not insert the diameter."
-  let area = (diameter) /2*Pi*2
-  let circumference = (diameter) * Pi 
+  if (isNaN(output)) {
+    output.textContent='You did not insert the diameter.'
+  let area = diameter / 2 * Pi * 2
+  let circumference = diameter * Pi 
     output.textContent = "Area =" + area
      output2.textContent = "Circumference = " +
      circumference
+  }
 }
